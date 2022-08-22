f1a789e4e92ad0fcc69a863aa732f2a5db55f7fe
Paulo <Paulovsaraty@gmail.com>
2022-08-22 18:50:24 -0300

    Camera now dynamically zooms in and out
    Placeholder textures added

--- Assets/FlappyWings/Scripts/CameraController.cs
+++ Assets/FlappyWings/Scripts/CameraController.cs
@@ -3,54 +3,89 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour{
-    Transform playerOne;
-    Vector3 gizmoPos;
-    public Vector3 offset;
+    Vector3 gizmoPos, gizmoPosMin, gizmoPosMax;
+    public Vector3 fixedOffset, dynamicOffset;
     public float smoothSpeed;
 
     void Start(){
-        StartCoroutine(CameraStartDelay());
+        //StartCoroutine(CameraStartDelay());
     }
 
     void FixedUpdate(){
-        if(playerOne != null && GameManager.instance.playerList.Count < 2){
-            Vector3 desiredPosition = playerOne.transform.position + offset;
+        if (GameManager.instance.playerList.Count == 1){
+            Vector3 desiredPosition = GameManager.instance.playerList[0].transform.position + fixedOffset;
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
             transform.position = smoothedPosition;
         }
-        else if (GameManager.instance.playerList.Count >= 2){
-            Vector3 desiredPosition = FindCentroid() + offset;
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        else if (GameManager.instance.playerList.Count > 1){
+            Vector3 desiredPosition = FindCentroid() + fixedOffset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition + FindDynamicOffset(), smoothSpeed * Time.deltaTime);
             transform.position = smoothedPosition;
             gizmoPos = FindCentroid();
+            gizmoPosMin = FindMinPos();
+            gizmoPosMax = FindMaxPos();
         }
     }
 
     Vector3 FindCentroid(){
-        var totalX = 0f;
-        var totalY = 0f;
-        var totalZ = 0f;
-
+        Vector3 centerPos = new Vector3(0, 0, 0);
         foreach(var player in GameManager.instance.playerList){
-            totalX += player.transform.parent.transform.position.x;
-            totalY += player.transform.parent.transform.position.y;
-            totalZ += player.transform.parent.transform.position.z;
+            centerPos += player.transform.parent.transform.position;
         }
+        centerPos /= GameManager.instance.playerList.Count;
 
-        var centerX = totalX / GameManager.instance.playerList.Count;
-        var centerY = totalY / GameManager.instance.playerList.Count;
-        var centerZ = totalZ / GameManager.instance.playerList.Count;
+        return centerPos;
+    }
 
-        return new Vector3(centerX, centerY, centerZ);
+    Vector3 FindMinPos(){
+        Vector3 minPos = new Vector3(0, 0, 0);
+        float minX = float.MaxValue;
+        float minY = float.MaxValue;
+        float minZ = float.MaxValue;
+        foreach(var player in GameManager.instance.playerList){
+            if(player.transform.parent.transform.position.x < minX){
+                minX = player.transform.parent.transform.position.x;
+            }
+            if(player.transform.parent.transform.position.y < minY){
+                minY = player.transform.parent.transform.position.y;
+            }
+            if(player.transform.parent.transform.position.z < minZ){
+                minZ = player.transform.parent.transform.position.z;
+            }
+        }
+        return new Vector3(minX, minY, minZ);
+    }
+
+    Vector3 FindMaxPos(){
+        Vector3 maxPos = new Vector3(0, 0, 0);
+        float maxX = float.MinValue;
+        float maxY = float.MinValue;
+        float maxZ = float.MinValue;
+        foreach(var player in GameManager.instance.playerList){
+            if(player.transform.parent.transform.position.x > maxX){
+                maxX = player.transform.parent.transform.position.x;
+            }
+            if(player.transform.parent.transform.position.y > maxY){
+                maxY = player.transform.parent.transform.position.y;
+            }
+            if(player.transform.parent.transform.position.z > maxZ){
+                maxZ = player.transform.parent.transform.position.z;
+            }
+        }
+        return new Vector3(maxX, maxY, maxZ);
+    }
+
+    Vector3 FindDynamicOffset(){
+        float distanceX, distanceZ;
+        
+        distanceX = FindMaxPos().x - FindMinPos().x;
+        distanceZ = FindMaxPos().z - FindMinPos().z;
+        return new Vector3(0, (distanceX + distanceZ) * 0.25f, (distanceX + distanceZ) * -0.25f);
     }
 
     private void OnDrawGizmos(){
         Gizmos.DrawCube(gizmoPos, new Vector3(1, 1, 1));
-    }
-
-    IEnumerator CameraStartDelay(){
-        yield return new WaitForSeconds(0.2f);
-        playerOne = GameManager.instance.playerList[0].gameObject.transform;
-        transform.position = playerOne.transform.position + offset;
+        Gizmos.DrawCube(gizmoPosMin, new Vector3(1, 1, 1));
+        Gizmos.DrawCube(gizmoPosMax, new Vector3(1, 1, 1));
     }
 }
--- Assets/FlappyWings/Scripts/GameManager.cs
+++ Assets/FlappyWings/Scripts/GameManager.cs
@@ -37,7 +37,7 @@ public class GameManager : MonoBehaviour
     }
 
     private void Start(){
-        PlayerInputManager.instance.JoinPlayer(0, -1, null);
+        //PlayerInputManager.instance.JoinPlayer(0, -1, null);
         //Debug.Log(PlayerJoinedGame);
     }
 
