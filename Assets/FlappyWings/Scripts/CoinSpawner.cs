9e95d3b292c814132bfe647cc11b4d11ba2ca8fb
Paulo <Paulovsaraty@gmail.com>
2022-08-22 11:16:06 -0300

    Modified spawner
    Conditions added, spawner is now its own gameObject

--- Assets/FlappyWings/Scripts/CoinSpawner.cs
+++ Assets/FlappyWings/Scripts/CoinSpawner.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CoinSpawner : MonoBehaviour{
     public GameObject[] coins;
+    public LayerMask layerMask;
 
     public float XMin, XMax, ZMin, ZMax;
 
     Vector3 spawnPosition;
+    Vector3 gizmoPos;
 
     [SerializeField] float minSpawnInterval = 1f, maxSpawnInterval = 3f;
     [SerializeField] bool spawnerEnabled = true;
@@ -19,9 +21,17 @@ public class CoinSpawner : MonoBehaviour{
     IEnumerator SpawnCoins(){
         while (spawnerEnabled){
             yield return new WaitForSeconds(Random.Range(minSpawnInterval, maxSpawnInterval));
-            if(GameManager.instance.playerList.Count > 1){
+
+            if(GameManager.instance.playerList.Count > 0){
                 Instantiate(coins[Random.Range(0, coins.Length)], RandomNewSpawnPosition(), transform.rotation);
             }
+
+            //Vector3 chosenPos = RandomNewSpawnPosition();
+            //if(Helper.SafeSpawnPoint(chosenPos, layerMask, 1f)){
+            //    Instantiate(coins[Random.Range(0, coins.Length)], chosenPos, transform.rotation);
+            //}
+
+
         }
     }
 
