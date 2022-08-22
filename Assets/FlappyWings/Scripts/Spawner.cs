cf9e7aec5225c6b9b04e830f24f469e319c65909
Paulo <Paulovsaraty@gmail.com>
2022-08-22 11:54:11 -0300

    Spawner is now generic

--- Assets/FlappyWings/Scripts/Spawner.cs
+++ Assets/FlappyWings/Scripts/Spawner.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinSpawner : MonoBehaviour{
-    public GameObject[] coins;
+public class Spawner : MonoBehaviour{
+    public GameObject[] objectsToBeSpawned;
     public LayerMask layerMask;
 
     public float XMin, XMax, ZMin, ZMax;
 
     Vector3 spawnPosition;
-    Vector3 gizmoPos;
+    //Vector3 gizmoPos;
 
     [SerializeField] float minSpawnInterval = 1f, maxSpawnInterval = 3f;
     [SerializeField] bool spawnerEnabled = true;
@@ -23,7 +23,7 @@ public class CoinSpawner : MonoBehaviour{
             yield return new WaitForSeconds(Random.Range(minSpawnInterval, maxSpawnInterval));
 
             if(GameManager.instance.playerList.Count > 0){
-                Instantiate(coins[Random.Range(0, coins.Length)], RandomNewSpawnPosition(), transform.rotation);
+                Instantiate(objectsToBeSpawned[Random.Range(0, objectsToBeSpawned.Length)], RandomNewSpawnPosition(), transform.rotation);
             }
 
             //Vector3 chosenPos = RandomNewSpawnPosition();
@@ -38,6 +38,6 @@ public class CoinSpawner : MonoBehaviour{
     Vector3 RandomNewSpawnPosition(){
         float randomX = Random.Range(XMin, XMax);
         float randomZ = Random.Range(ZMin, ZMax);
-        return new Vector3(randomX, this.transform.position.y, randomZ);
+        return new Vector3(this.transform.position.x + randomX, this.transform.position.y, this.transform.position.z + randomZ);
     }
 }
